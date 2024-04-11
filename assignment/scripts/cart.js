ec4f841a9f56f7a9c62ffc4bcda481757b825233
Patrick Davison <davisonpatrickj@gmail.com>
2024-04-10 19:27:27 -0500

    work through problem 3

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,14 +2,32 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+let basket = ['Nails', 'Screws'];
 
+function addItem(item){
+   basket.push(item)
+    return true;
+}
+addItem('Hammer');
+console.log(basket);
 
+ function listItems(basket){
+    for (let item of basket){
+          console.log('basket', item);  
+     }
+ };
+listItems(basket);
 
+// function listItems(basket){
+//     for (let i=0; i > basket.legth; i ++);
+//     console.log('basket:', basket);
+// }
+// console.log(listItems(basket));
 
-
-
-
-
+function empty() {
+    for (i= basket.length; i =0; i--);
+    console.log(basket);
+}
 
 
 // DO NOT MODIFY
